0094735b305284228026aa98a5e2c43c04ca1748
affordaweb <affordaweb@gmail.com>
2026-04-04 21:30:47 -0700

    feat: configure Next.js image domains for Facebook CDN

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'scontent.fmnl30-1.fna.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.fmnl30-2.fna.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.fmnl30-3.fna.fbcdn.net',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
